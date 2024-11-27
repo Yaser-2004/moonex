7286c4ad2ae70ea6513eb5de785f1cc04d5a1845
Yaser Siddiquee <yaser.siddiquee@gmail.com>
2024-11-27 16:48:41 +0530

    loader corrected

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,24 @@
-import { useEffect, useState } from 'react'
+import { Suspense, lazy } from 'react'
 import './App.css'
-import FAQ from './Section/FAQ/FAQ'
-import Footer from './Section/Footer/Footer'
-import HeroS from './Section/HeroS/HeroS'
-import Section2 from './Section/Section2/Section2'
-import Section3 from './Section/Section3/Section3'
-import Loader from './Components/Loader/Loader'
+
+const FAQ = lazy(() => import('./Section/FAQ/FAQ'))
+const Footer = lazy(() => import('./Section/Footer/Footer'))
+const HeroS = lazy(() => import('./Section/HeroS/HeroS'))
+const Section2 = lazy(() => import('./Section/Section2/Section2'))
+const Section3 = lazy(() => import('./Section/Section3/Section3'))
+const Loader = lazy(() => import('./Components/Loader/Loader'))
 
 function App() {
 
-  const [isLoading, setIsLoading] = useState(true);
-
-  useEffect(() => {
-    const handleLoad = () => {
-      setIsLoading(false);
-    };
-
-    // Listen for when the entire page has loaded
-    window.addEventListener("load", handleLoad);
-
-    return () => {
-      window.removeEventListener("load", handleLoad);
-    };
-  }, []);
-
   return (
     <div className='box-border m-0 p-0'>
-      {isLoading ? <Loader /> : 
-        <>
-          <HeroS />
-          <Section2 />
-          <Section3 />
-          <FAQ />
-          <Footer />
-        </>
-      }
+      <Suspense fallback={<Loader />}>
+        <HeroS />
+        <Section2 />
+        <Section3 />
+        <FAQ />
+        <Footer />
+      </Suspense>
     </div>
   )
 }
